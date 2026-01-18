30db853130bd4bf6a4cbf44384e20e09e6b4f7f7
Soumya Jain <soumya09062006@gmail.com>
2026-01-18 21:27:20 +0530

    fixing bugs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/dressup/', // ← ADD THIS LINE
+  plugins: [react()]
 })
